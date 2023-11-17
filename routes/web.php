5d24c4732ab99b223e809b05caf09d94a0aa44e0
abby2810 <abeergandhi2@gmail.com>
2023-11-17 16:10:55 +0530

    Add hello world code

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,14 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+/* Route::get('/veggies/baigan', function () {
+    return 'Baigan';
+});*/
+
+Route::get('/veggies/{veggieName}', function (string $veggiename) {
+        return $veggiename;
 });
